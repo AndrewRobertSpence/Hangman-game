26e2bb10d205df1371c15771e5703dc9de910e92
Andrew Spence <aspence3595@gmail.com>
2024-04-04 09:36:52 +1300

    updated JS, tidied up pseudo-code

--- scripts/hangman.js
+++ scripts/hangman.js
@@ -1,13 +1,11 @@
-// Data accessed from wordlist.js.
-// Object properties: word, hint
-
-// Set Dom elements as variables
+// 1. Data accessed from wordlist.js.
+// 2. Object properties: word, hint
+// 3. Set Dom elements as variables
 let gameIntroPage = document.getElementById("game__intro__page");
-// let gameHeading = document.getElementById("game__heading");
 let gameContent = document.getElementById("game__content");
 let gameOutcome = document.getElementById("game__outcome");
 let gameBestScores = document.getElementById("game__outcome__bestscores");
-let playAgainButton = document.querySelector(".game__playagain__button"); // Updated class name here
+let playAgainButton = document.querySelector(".game__playagain__button");
 let hangmanImage = document.querySelector(".game__display__img");
 let wordDisplay = document.querySelector(".game__display__word");
 let hintDisplay = document.querySelector(".game__display__hint span");
@@ -27,12 +25,12 @@ let gameOutcomeText = document.getElementById("game__outcome__text");
 let tableBody = document.querySelector("#game__outcome__bestscores tbody");
 let randomNumber = Math.floor(Math.random() * wordList.length);
 
-// Set random word and hint
+// 5. Set random word and hint
 let randomWord = wordList[randomNumber].word;
 let randomHint = wordList[randomNumber].hint;
 console.log(randomWord, randomHint);
 
-// Initiate variables for randomWord, wordGuesses, maximumAmountOfGuesses, avatars, alphabet
+// 6. Initiate variables for randomWord, wordGuesses, maximumAmountOfGuesses, avatars, alphabet
 let amountOfGuesses = 0;
 let maximumAmountOfGuesses = 6;
 let guessedLetters = [];
@@ -84,7 +82,7 @@ let alphabet = [
   "z",
 ];
 
-// Function wordToGuessUnderlines
+// 7. Function wordToGuessUnderlines
 const wordToGuessUnderlines = () => {
   wordDisplay.innerHTML = randomWord
     .split("")
@@ -96,153 +94,150 @@ const wordToGuessUnderlines = () => {
     })
     .join("");
 };
-// Function createKeyboardButtons
+
+// 8. Function createKeyboardButtons
 const createKeyboardButtons = (alphabet) => {
   for (let letter of alphabet) {
     const button = document.createElement("button");
     button.innerHTML = letter;
     button.classList.add("keyboard__button");
     button.addEventListener("click", (e) => {
-      // Add 'active' class to the clicked button
+      // 8.1 Add 'active' class to the clicked button
       e.target.classList.add("active");
-      // startGame(e.target, letter);
     });
     keyboardContainer.appendChild(button);
   }
 };
-// call the createKeyboardButtons function
+
+// 9. Call the createKeyboardButtons function
 createKeyboardButtons(alphabet);
 
 const keyboardButtons = document.querySelectorAll(".keyboard__button");
 keyboardButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    // Get the clicked letter from the button's text content
+    // 9.1 Get the clicked letter from the button's text content
     const letter = button.textContent.toLowerCase();
-    // Call the guessLetter function with the clicked letter
+    // 9.2 Call the guessLetter function with the clicked letter
     guessLetter(letter);
-    // Disable the button after it's clicked
+    // 9.3 Disable the button after it's clicked
     button.disabled = true;
   });
 });
 
-// function for guessLetter
-// Function to handle letter guesses
+// 10. function for guessLetter
 const guessLetter = (letter) => {
-  // Check if the guessed letter exists in the random word
+  // 10.1 Check if the guessed letter exists in the random word
   const wordLetters = randomWord.split("");
   let letterFound = false;
-
   wordLetters.forEach((char, index) => {
     if (char === letter) {
-      // If the letter exists and has not been guessed yet, update the display
+      // 10.2 If the letter exists and has not been guessed yet, update the display
       if (!guessedLetters.includes(letter)) {
         const letterSpan = wordDisplay.querySelectorAll(".letterSpan")[index];
         letterSpan.textContent = letter;
         letterFound = true;
+        letterSpan.classList.add("flip");
       }
     }
   });
-  // If the guessed letter is not in the word, increment the guesses counter and update display
+  // 10.3 If the guessed letter is not in the word, increment the guesses counter and update display
   if (!letterFound) {
     amountOfGuesses++;
     displayGuesses(amountOfGuesses);
     displayImage(amountOfGuesses);
   }
-  // Add the guessed letter to the array of guessed letters
+  // 10.4 Add the guessed letter to the array of guessed letters
   if (!guessedLetters.includes(letter)) {
     guessedLetters.push(letter);
   }
-  // Check for win condition after updating display
+  // 10.5 Check for win condition after updating display
   checkWin();
 };
-
+// 11. function for createAvatarButtons
 const createAvatarButtons = (avatars) => {
   for (let i = 0; i < avatars.length; i++) {
     const button = document.createElement("button");
-    // Accessing avatar paths
+    // 11.1 Accessing avatar paths
     const avatarPath = avatars[i].path;
     button.innerHTML = `<img class="avatarImage" src="${avatarPath}" alt="avatar image">`;
     button.addEventListener("click", () => {
-      // Remove the 'active' class from all avatar buttons
+      // 11.2 Remove the 'active' class from all avatar buttons
       allAvatarButtons.forEach((btn) => {
         btn.classList.remove("active");
       });
-      // Add the 'active' class to the clicked button
+      // 11.3 Add the 'active' class to the clicked button
       button.classList.add("active");
-      // Apply grayscale to all avatar images except the clicked one
+      // 11.4 Apply grayscale to all avatar images except the clicked one
       const allAvatarImages = document.querySelectorAll(".avatarImage");
       allAvatarImages.forEach((img) => {
         if (img !== button.querySelector("img")) {
           img.style.filter = "grayscale(100%)";
         } else {
-          // Reset filter for the clicked image
+          // 11.5 Reset filter for the clicked image
           img.style.filter = "none";
         }
       });
-      // Call function to update the profile with the selected avatar
+      // 11.6 Call function to update the profile with the selected avatar
       createUserAvatar(avatarPath);
     });
-    // Add a class to all avatar buttons
+    // 11.7 Add a class to all avatar buttons
     button.classList.add("avatarButton");
     avatar.appendChild(button);
   }
 };
 createAvatarButtons(avatars);
 
-// update the profile of the player
+// 12. Update the profile of the player
 const createUserAvatar = (avatarPath) => {
   headingAvatar.innerHTML = `<img class="avatarImage" src="${avatarPath}" alt="avatar image">`;
 };
 
-// function for if Avatar is clicked
+// 13. Function for if Avatar is clicked
 const isAvatarClicked = () => {
   const activeAvatarButton = document.querySelector(".avatarButton.active");
   return activeAvatarButton !== null;
 };
 
-// Function createUsername
+// 14. Function createUsername
 const createUsername = () => {
   const enteredUsername = username.value;
   headingUsername.innerHTML = enteredUsername;
 };
 
-// function Display image
+// 15. Function Display image
 const displayImage = (amountOfGuesses) => {
   hangmanImage.src = `public/hangman-${amountOfGuesses}-white.png`;
 };
 
-// function Display hint
+// 16. Function Display hint
 const displayHint = (randomHint) => {
   hintDisplay.innerHTML = randomHint;
 };
 
-// function Display guesses
+// 17. Function Display guesses
 const displayGuesses = (amountOfGuesses) => {
   guessDisplay.innerHTML = amountOfGuesses;
 };
 
-// JavaScript code to hide gameOutcome, gameHeading and gameContent elements
+// 18. JavaScript code to hide gameOutcome, gameHeading and gameContent elements
 gameContent.style.display = "none";
-// gameHeading.classList.add("hidden");
 console.log(gameContent.classList.contains("hidden"));
-// console.log(gameHeading.classList.contains("hidden"));
 
-// function Start game
+// 19. Function Start game
 const startGame = () => {
   if (username.value === "") {
     alert("Please enter a username");
-    // Exit the function if no username is entered
+    // 19.1 Exit the function if no username is entered
     return;
   }
   if (!isAvatarClicked()) {
     alert("Please select an avatar before starting the game.");
-    // Exit the function if no avatar is clicked
+    // 19.2 Exit the function if no avatar is clicked
     return;
   }
-  // gameHeading.classList.remove("hidden");
-  // Hide the game intro page
+  // 19.3 Hide the game intro page
   gameIntroPage.style.display = "none";
-  // Show the game content
+  // 19.4 Show the game content
   gameContent.style.display = "block";
   amountOfGuesses = 0;
   wordToGuessUnderlines();
@@ -254,134 +249,121 @@ const startGame = () => {
 
 startButton.addEventListener("click", startGame);
 
-// Reset the game when the "Play Again" button is clicked
+// 20. Reset the game when the "Play Again" button is clicked
 const resetGame = () => {
   console.log("reset game");
-
-  // Clear the guessed letters array
+  // 20.1 Clear the guessed letters array
   guessedLetters = [];
 
-  // Reset the guessed letters display
+  // 20.2 Reset the guessed letters display
   letterSpans.forEach((span) => {
     span.textContent = "";
   });
-
-  // Reset the hangman image
+  // 20.3 Reset the hangman image
   amountOfGuesses = 0;
   displayGuesses(amountOfGuesses);
   displayImage(amountOfGuesses);
-
-  // Clear the input field
+  // 20.4 Clear the input field
   username.value = "";
-
-  // Remove the existing avatar buttons and recreate them
+  // 20.5 Remove the existing avatar buttons and recreate them
   avatar.innerHTML = ""; // Remove all child nodes
   createAvatarButtons(avatars);
-
-  // Reset the avatar selection and remove grayscale filter
+  // 20.6 Reset the avatar selection and remove grayscale filter
   allAvatarImages.forEach((img) => {
-    img.style.filter = "none"; // Remove grayscale filter
+    // 20.7 Remove grayscale filter
+    img.style.filter = "none";
   });
   headingAvatar.innerHTML = "";
-
-  // Reset the outcome text and hide it
+  // 20.8 Reset the outcome text and hide it
   gameOutcomeText.innerHTML = "";
-
-  // Show the intro page and hide other sections
+  // 20.9 Show the intro page and hide other sections
   gameIntroPage.style.display = "block";
-  // gameHeading.classList.add("hidden");
+  // 20.10 gameHeading.classList.add("hidden");
   gameContent.style.display = "none";
   gameOutcome.classList.remove("visible");
-
-  // Reset the random word and hint
+  // 20.11 Reset the random word and hint
   randomNumber = Math.floor(Math.random() * wordList.length);
   randomWord = wordList[randomNumber].word;
   randomHint = wordList[randomNumber].hint;
-
-  // Reset word display with new random word
+  // 20.12 Reset word display with new random word
   wordToGuessUnderlines();
-
-  // Display new hint
+  // 20.13 Display new hint
   displayHint(randomHint);
-
-  // Reset keyboard buttons
+  // 20.14 Reset keyboard buttons
   keyboardButtons.forEach((button) => {
     button.disabled = false;
     button.classList.remove("active");
   });
 };
 
-// document.addEventListener('DOMContentLoaded', function() {
-//   console.log("DOM loaded");
 playAgainButton.addEventListener("click", resetGame);
-// });
 
+// 21. Function checkWin
 const checkWin = () => {
   console.log("Checking win...");
   const wordLetters = randomWord.split("");
   const guessedLetters = Array.from(
     wordDisplay.querySelectorAll(".letterSpan")
   ).map((span) => span.textContent);
-
-  // Check if all letters in the word have been guessed
+  // 21.1 Check if all letters in the word have been guessed
   if (wordLetters.every((letter) => guessedLetters.includes(letter))) {
     console.log("Player wins!");
     gameOutcomeImage.src = "public/Congratulations.gif";
-    // Call createProfile function to add the winner to the best scores
+    // 21.2 Call createProfile function to add the winner to the best scores
     createProfile();
-    // Delay transition to game outcome screen after 2 seconds if game is won
+    // 21.3 Delay transition to game outcome screen after 2 seconds if game is won
     setTimeout(() => {
       console.log("Showing game outcome...");
-      // gameHeading.classList.add("hidden");
       gameContent.style.display = "none";
       gameOutcome.classList.add("visible");
-    }, 2000); // 2000 milliseconds delay (2 seconds)
+    }, 2000); 
   }
-  // Check if maximum number of guesses reached
+  // 21.4 Check if maximum number of guesses reached
   if (amountOfGuesses === maximumAmountOfGuesses) {
     console.log("Maximum guesses reached.");
     gameOutcomeImage.src = "public/Try-Again.gif";
     gameOutcomeText.innerHTML = `The correct answer was:  <span>${randomWord}</span>`;
-    // Delay transition to game outcome screen after 2 seconds if maximum guesses reached
+    // 21.5 Delay transition to game outcome screen after 2 seconds if maximum guesses reached
     setTimeout(() => {
       console.log("Showing game outcome...");
-      // gameHeading.classList.add("hidden");
-      gameContent.style.display = "none";
+       gameContent.style.display = "none";
       gameOutcome.classList.add("visible");
-    }, 2000); // 2000 milliseconds delay (2 seconds)
+    }, 2000); 
   }
 };
 
+// 22. Function createProfile
 const createProfile = () => {
-  // Get current date
+  // 22.1 Get current date
   const currentDate = new Date().toLocaleDateString();
-  // Get selected avatar path
+  // 22.2 Get selected avatar path
   const selectedAvatar = document.querySelector(".avatarButton.active img").src;
-  // Extract the part of the avatar path starting from "public"
+  // 22.3 Extract the part of the avatar path starting from "public"
   const avatarPath = selectedAvatar.substring(selectedAvatar.indexOf("public"));
-  // Get entered username
+  // 22.4 Get entered username
   const enteredUsername = username.value;
-  // Create profile object
+  // 22.5 Create profile object
   const profile = {
     date: currentDate,
     avatar: avatarPath,
     username: enteredUsername,
     guesses: amountOfGuesses,
   };
-  // Add profile to profiles array
+  // 22.6 Add profile to profiles array
   profiles.push(profile);
-  // Sort profiles by number of guesses (ascending)
+  // 22.7 Sort profiles by number of guesses (ascending)
   profiles.sort((a, b) => a.guesses - b.guesses);
-  // Update best scores table
+  // 22.8 Update best scores table
   updateBestScoresTable();
-  // Optionally, you can log the created profile for debugging
+  // 22.9 Optionally, you can log the created profile for debugging
   console.log("New Profile:", profiles);
 };
 
+// 23. Function updateBestScoresTable
 const updateBestScoresTable = () => {
-  // Clear existing rows
+  // 23.1 Clear existing rows
   tableBody.innerHTML = "";
-  // Add up to 10 best scores to the table
+  // 23.2 Add up to 10 best scores to the table
   profiles.slice(0, 10).forEach((profile) => {
     const row = `
       <tr class="bestscore__row">
